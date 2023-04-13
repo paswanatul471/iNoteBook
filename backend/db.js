a3ee92a5326407eec7fcb7fb9eed12e77fc7260b
Atul Paswan <90141410+paswanatul471@users.noreply.github.com>
2023-04-13 11:04:14 +0530

    Uploaded

---
+++ backend/db.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const mongooseURI = "mongodb+srv://paswanatul07:Atul0786@cluster0.cvfnvay.mongodb.net/techycricketinotebook?retryWrites=true&w=majority"
+// const mongooseURI = "mongodb://localhost:27017/inotebook"
+
+const connectTomongo = ()=>{
+    mongoose.connect(mongooseURI,()=>{
+        console.log("Connected to mongo succesfull");
+    })
+    
+};
+module.exports = connectTomongo;
---
+++ backend/index.js
@@ -0,0 +1,14 @@
+const connectTomongo = require('./db');
+const express = require('express')
+connectTomongo();
+
+const app = express()
+const port = 5000
+app.use(express.json());
+// Available Routes
+
+app.use('/api/auth',require('./routes/auth'))
+app.use('/api/notes',require('./routes/notes'))
+app.listen(port, () => {
+  console.log(`Example app listening at port http://localhost:${port}`)
+})
