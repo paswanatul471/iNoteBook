59f8b64d6bf967a8bcbfc79098e871221ad03d9e
Atul Paswan <90141410+paswanatul471@users.noreply.github.com>
2023-12-03 01:49:28 +0530

    Update db.js
    hide username and password of database.

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongooseURI = "mongodb+srv://paswanatul07:Atul0786@cluster0.cvfnvay.mongodb.net/techycricketinotebook?retryWrites=true&w=majority"
+const mongooseURI = "mongodb+srv://<username>:<password>@cluster0.cvfnvay.mongodb.net/techycricketinotebook?retryWrites=true&w=majority"
 
 const connectTomongo = ()=>{
     mongoose.connect(mongooseURI,()=>{
